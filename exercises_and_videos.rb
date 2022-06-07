4d0d5c10f774f1b7311e439e517756d3453988fc
Kirill Bogdanov <zizus89@gmail.com>
2022-06-07 17:12:05 +0500

    add v2 fitness bot

---
+++ exercises_and_videos.rb
@@ -0,0 +1,19 @@
+module ExercisesEndVideos
+  def choice_exercises
+    {
+      'махи гири' => 'https://youtu.be/dZKPJhVbcTw',
+      'горизонтальная тяга' => 'https://youtu.be/0-PzqaAb_ls',
+      'вертикальная тяга' => 'https://youtu.be/gSFc9eU6wH4',
+      'отжимания' => 'https://youtu.be/aAh8XmiwV4g',
+      'горизонтальный жим' => 'https://youtu.be/XUeE5cTzsfo',
+      'отведение бедра' => 'https://youtu.be/Er3jBZd_NQA',
+      'приседаниея с вертикальным жимом гантелей' => 'https://youtu.be/bvLIuwPySSI',
+      'тяга кинга' => 'https://youtu.be/wSC8e2-sF9c',
+      'становая тяга на одной ноге' => 'https://youtu.be/retxH99krJA',
+      'приведение бедра' => 'https://youtu.be/TGW_c-9XVYQ',
+      'разгибания голени' => 'https://youtu.be/IEM5z5OQ-mo',
+      'cгибания голени' => 'https://youtu.be/Dz1XFU3WSbQ',
+      'жим платформы одной ногой' => 'https://youtu.be/LL0uQJthemo'
+    }
+  end
+end
--- main.rb
+++ main.rb
@@ -1,14 +1,5 @@
 require 'telegram/bot'
 require 'json'
 
-require_relative 'programs'
-include WorkoutProgram
-
-require_relative 'starting_salutation'
-include StartDesc
-
 require_relative 'main_logic'
 include MainLogic
-
-# require_relative 'videos_and_workout'
-# include Exercises
